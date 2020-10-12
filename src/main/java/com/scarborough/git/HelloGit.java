f183d5d55e6eed71a518576a1ae2254e94dbd223
MssingBefore <18723951990@163.com>
2020-10-12 19:22:57 +0800

    分支

--- src/main/java/com/scarborough/git/HelloGit.java
+++ src/main/java/com/scarborough/git/HelloGit.java
@@ -2,9 +2,12 @@ package com.scarborough.git;
 //team->add index	添加到暂存区
 //team->commit	提交
 //git->添加忽略.gitigore
+//team->show history	返回历史版本	命令行才能到新的
+//team->switch to	分支
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("更新1");
+		System.out.println("创建分支");
 	}
 }
