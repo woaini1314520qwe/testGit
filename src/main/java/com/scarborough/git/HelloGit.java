a960ee8e831325a595ce88c5632b69ee51b487a3
MssingBefore <18723951990@163.com>
2020-10-12 19:16:35 +0800

    添加文件

---
+++ src/main/java/com/scarborough/git/HelloGit.java
@@ -0,0 +1,9 @@
+package com.scarborough.git;
+//team->add index	添加到暂存区
+//team->commit	提交
+//git->添加忽略
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+	}
+}
