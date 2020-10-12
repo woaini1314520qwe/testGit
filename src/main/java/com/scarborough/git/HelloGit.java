3f8fe63fdd6f08959976bbdb56db2570516f08bf
MssingBefore <18723951990@163.com>
2020-10-12 19:18:14 +0800

    跟新1

--- src/main/java/com/scarborough/git/HelloGit.java
+++ src/main/java/com/scarborough/git/HelloGit.java
@@ -1,9 +1,10 @@
 package com.scarborough.git;
 //team->add index	添加到暂存区
 //team->commit	提交
-//git->添加忽略
+//git->添加忽略.gitigore
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("更新1");
 	}
 }
