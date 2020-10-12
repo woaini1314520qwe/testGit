4fbffaffdb42ed7c71ce4c10576eb4175f59fc76
MssingBefore <18723951990@163.com>
2020-10-12 19:26:06 +0800

    主干添加

--- src/main/java/com/scarborough/git/HelloGit.java
+++ src/main/java/com/scarborough/git/HelloGit.java
@@ -3,11 +3,12 @@ package com.scarborough.git;
 //team->commit	提交
 //git->添加忽略.gitigore
 //team->show history	返回历史版本	命令行才能到新的
-//team->switch to	分支
+//team->switch to	分支		合并分支-->merge
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("更新1");
 		System.out.println("创建分支");
+		System.out.println("主干添加");
 	}
 }
